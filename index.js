0633dc5421b3994d9831ba89663e88660381852d
Jashanpreet Singh <708424@student.tor.loc>
2019-08-07 16:47:31 -0400

    second commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const data = {
+firstName: 'Jashanpreet',
+lastname: 'Singh'
+}
+
+const site = http.createServer(function(req, res) {
+console.log('Hello World');
+console.log(req.headers);
+console.log(req.url)
+res.setHeader('Content-Type', 'text/html');
+res.write(JSON.stringify(data));
+res.end();
+});
+site.listen(3000);
