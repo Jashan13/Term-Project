6aca0b8aae7e445491bcee0022275ec52b7c90f0
Jashanpreet Singh <708424@student.tor.loc>
2019-08-07 16:50:48 -0400

    third commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 const data = {
 firstName: 'Jashanpreet',
 lastname: 'Singh'
@@ -12,4 +12,14 @@ res.setHeader('Content-Type', 'text/html');
 res.write(JSON.stringify(data));
 res.end();
 });
-site.listen(3000);
+site.listen(3000);*/
+
+
+var express = require('express');
+var app = express()
+
+app.get('/',function(req,res){
+res.send('helooo there')
+})
+
+app.listen(3000)
